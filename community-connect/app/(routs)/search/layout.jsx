09fff9ae4cbcc9ae6b88a2cf0d64e81029f10687
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2024-12-21 19:58:12 +0200

    Update layout.jsx

--- community-connect/app/(routs)/search/layout.jsx
+++ community-connect/app/(routs)/search/layout.jsx
@@ -3,16 +3,15 @@ import CategoriesSideBar from "./_components/CategoriesSideBar";
 
 function layout({ children }) {
   return (
-    <div className="min-h-screen" >
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 mt-8 gap-2 lg:mx-12 md:mx-4">
-        <div className="hidden md:block col-span-1">
-          {/* Side Category Nav Bar */}
-          <CategoriesSideBar />
-        </div>
-        <div
-          className="md:col-span-3 min-h-screen "
-        >
-          {children}
+    <div className="flex flex-col min-h-screen">
+      {/* Main Content */}
+      <div className="flex-grow">
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 mt-8 gap-2 lg:mx-12 md:mx-4">
+          <div className="hidden md:block col-span-1">
+            {/* Side Category Nav Bar */}
+            <CategoriesSideBar />
+          </div>
+          <div className="md:col-span-3">{children}</div>
         </div>
       </div>
     </div>
