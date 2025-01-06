bd36b22d9e8defc1e5728c6c159f80e00bd5e9f4
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2025-01-06 09:59:41 +0200

    Update Footer.jsx

--- community-connect/app/_components/Footer.jsx
+++ community-connect/app/_components/Footer.jsx
@@ -1,15 +1,15 @@
-// import React from "react";
+import React from "react";
 
-// function Footer() {
-//   return (
-//     <footer className="bg-green-100 text-primary italic font-bold py-4 border-t-2 border-gray-200 shadow-md w-full">
-//       <div className="container mx-auto text-center">
-//         <p>
-//           © {new Date().getFullYear()} Community Connect. All rights reserved.
-//         </p>
-//       </div>
-//     </footer>
-//   );
-// }
+function Footer() {
+  return (
+    <footer className="bg-green-100 text-primary italic font-bold py-4 border-t-2 border-gray-200 shadow-md w-full">
+      <div className="container mx-auto text-center">
+        <p>
+          © {new Date().getFullYear()} Community Connect. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
 
-// export default Footer;
+export default Footer;
